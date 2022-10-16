c83606be78affbdfe8d920fc60e09e56c24c0759
Jose Henrique Luckmann <jose.luckmann@ambevtech.com.br>
2022-10-16 17:39:58 -0300

    Auto config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 import string
-
+import random
 from decouple import config
 
 
@@ -8,11 +8,9 @@ BASE_DIR = os.path.abspath('.')
 
 DEBUG = True#config('DEBUG', cast=bool)
 
-SECRET_KEY = config('SECRET_KEY') or \
-    ''.join(random.choice(string.ascii_letters) for i in range(42))
+SECRET_KEY = ''.join(random.choice(string.ascii_letters) for i in range(42))
 
-SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI') or \
-    'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', True, cast=bool)
 
